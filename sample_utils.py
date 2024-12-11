7541483810f69dcba174066790c56944913ac136
Garo Bedonian <garo.bedonian@austin.utexas.edu>
2024-12-10 19:43:59 -0600

    added variable scaling tools, finished sobol indices

--- sample_utils.py
+++ sample_utils.py
@@ -1,50 +1,108 @@
 from scipy.stats.qmc import scale
+from scipy.stats import norm, uniform, beta, lognorm
 import numpy as np
 from sobol_tools import *
 
+
+
+dist_map = {
+    "normal": norm,
+    "uniform": uniform,
+    "beta": beta,
+    "lognormal"; lognorm
+}
+
 #------------------------------------------------------------------------------
-
 """
-Class for managing sample inputs between categorized dictionaries and flattened arrays
+Class for managing sample data between categorized dictionaries and flattened arrays
 
+Intended to be used for either inputs or outputs
 """
 class SampleData():
     
 
-    def __init__(self, categories, sizes, precomp=None):
+    def __init__(self, categories, sizes, scales=None, precomp=None):
 
         #NOTE: precomp dimensions should override sizes, or at least assert
-
         assert len(categories) == len(sizes)
 
         self.data = {}
         self.inds = {}
+        self.inds_inv = {}
+        self.scales = {}
 
         # initialize and generate full array indices
         c  = 0
-        for cat, size in categories, sizes:
-            self.data[cat] = np.array([size, 0])
+        for cat, size in zip(categories, sizes):
+            self.data[cat] = np.empty([size, 0])
             self.inds[cat] = list(range(c, c+size))
+            
+            #NOTE: inds dictionary entries should be unique, so inverting should work
+            c2 = 0
+            for ind in self.inds[cat]:
+                self.inds_inv[ind] = [cat, c2]
+                c2+=1
+
             c = c+size
 
 
         # if we have precomputed data in a similar dict, try adding it now
-        if precomp is isinstance(dict):
+        if isinstance(precomp, dict):
             self.addData(precomp)
 
         # if precomp is a numpy array
-        if precomp is isinstance(np.ndarray):
+        if isinstance(precomp, np.ndarray):
             self.addDataArray(precomp)
+
+        # possible references to either inputs that generated this data, or outputs generated from this data
+        self.input_ref = None
+        self.output_ref = None
+
 
 #------------------------------------------------------------------------------
 
-    def createData(self, N, method='sobol'):
+    def __call__(self, i, cat=None):
+
+        #return sample i, which may be a list
+        
+        # return full dict if category unspecified
+        if cat is None:    
+            sample = {}
+
+            for key, item in self.data:
+                sample[key] = self.data[key][:, i]
+
+            return sample
+        
+        # otherwise return array
+        else:
+            return self.data[cat][:,i]
+#------------------------------------------------------------------------------
+
+    def createData(self, N, scale=None, method='sobol'):
         # generate samples internally
         ndim = self.getNDim()
         
-        add = sobolSampleGen(ndim, N)
+        #TODO: Implement other methods
+        add = sobolSampleGen(ndim, N).T
+
+        # scale is dict
+        if scale is not None and self.scales is None:
+            self.scales = scale
+            add_scale = uniformToDist(add, self.scales, self.inds)
+        elif self.scales is not None:
+            if scale is not None:
+                print("Distribution mapping already present")
+
+            add_scale = uniformToDist(add, self.scales, self.inds)
+            
+
+        # uniform hypercube
+        else:
+            add_scale = add
+
+        self.addDataArray(add_scale)
         
-        self.addDataArray(add)
 #------------------------------------------------------------------------------
 
     def addData(self, add):
@@ -72,9 +130,9 @@ class SampleData():
 
     def addDataArray(self, add):
         # NOTE: added array indexing should match self.inds, this is a riskier approach
-        assert add is isinstance(np.ndarray)
+        assert isinstance(add, np.ndarray)
 
-        for key, ind in self.inds:
+        for key, ind in self.inds.items():
             self.data[key] = np.append(self.data[key], add[ind, :], axis=1)
 
 #------------------------------------------------------------------------------
@@ -119,12 +177,12 @@ class SampleData():
 
             ndim = 0
             for k in data.keys():
-                ndim += len(list(data[k][0]))
+                ndim += data[k].shape[0]
             return ndim
             
         # features in key entry
         else:
-            return len(list(data[key][0]))
+            return data[key].shape[0]
 
 #------------------------------------------------------------------------------
 
@@ -132,13 +190,116 @@ class SampleData():
         """
         Given the current sample data as A, generate 
         """
+        cats = self.getCategories()
+        sizes = [self.getNDim(x) for x in cats]
 
         # SD_A is the current instance
         SD_A = self
 
-        SD_B = SampleData()
+        # independent sample for B
+        SD_B = SampleData(cats, sizes, precomp=sobolSampleGen(self.getNDim(), self.getNSamples()).T)
+        # now produce SD_AB as a lambda function with __call__ similar to SampleData
+        SD_AB = lambda x, cat=None : _sobolABGen(x, SD_A, SD_B, cat)
+        # SD_AB = ABSamples(SD_A, SD_B)
 
 
         return SD_A, SD_B, SD_AB
     
+
+
+# #------------------------------------------------------------------------------
+
+# """
+# Class that specifically manages the AB samples of a Sobol sensitivity analysis
+
+# """
+# class ABSamples(SampleData):
+
+#     def __init__(self, SD_A, SD_B):
+#         self.SD_A = SD_A
+#         self.SD_B = SD_B
+
+#         # all we need is to set up the outputs, the inputs are handled in __call__
+
+
+
+
+#     def __call__(self, i, cat=None):
+
+#         #return sample i of the AB sample set, which may be a list
+#         return _sobolABGen(i, self.SD_A, self.SD_B, cat=cat)
+
 #------------------------------------------------------------------------------
+
+
+def _sobolABGen(i, SD_A, SD_B, cat):
+    # helper for generating the AB samples
+
+    N = SD_A.getNSamples()
+
+    # grabbing B based on standard AB order
+    A_ind = [j % N for j in list(i)]
+    B_ind = [j // N for j in list(i)]
+
+    #return sample i, which may be a list
+        
+    sample = {}
+
+    for key in SD_A.data:
+        sample[key] = SD_A.data[key][:, A_ind]
+
+    # replace B entries
+    B = SD_B.getInputArray()
+
+    #TODO: finish this up
+    for k in range(len(A_ind)):
+    # for k in A_ind:
+        cat_inv, ind_inv = SD_A.inds_inv[B_ind[k]]
+        sample[cat_inv][ind_inv][k] = SD_B.data[cat_inv][ind_inv][A_ind[k]]
+
+    
+    # return full dict if category unspecified
+    if cat is None:
+        return sample
+    # otherwise return array
+    else:
+        return sample[cat]
+    
+
+# use ppf to transform low discrepancy sequences like sobol to distributions
+def uniformToDist(data, scales, inds):
+
+    assert list(scales.keys()) == list(inds.keys())
+
+    data_s = np.zeros_like(data)
+
+    # iterate over indices
+    for key, ind, in inds:
+        scale_info = scales[key]
+
+        if isinstance(scale_info["loc"], list):
+            for i in range(len(ind)):
+
+                data_s[ind[i], :] = transformDist(data_s[ind[i], :], 
+                                scale_info['dist'][i], scale_info['loc'][i], scale_info['scale'][i])
+
+        else: # apply same dist to all variables in category
+
+            for i in range(len(ind)):
+
+                data_s[ind[i], :] = transformDist(data_s[ind[i], :], 
+                                scale_info['dist'], scale_info['loc'], scale_info['scale'])
+                
+    return data_s
+
+
+
+# NOTE: no beta dist capabilities yet
+def transformDist(x, dist, loc, scale, a=None, b=None):
+
+    if a is not None and b is not None:
+        x_s = dist_map[dist].ppf(x, loc=loc, scale=scale, a=a, b=b)
+    else:
+        x_s = dist_map[dist].ppf(x, loc=loc, scale=scale)
+
+    return x_s
--- sobol_tools.py
+++ sobol_tools.py
@@ -1,6 +1,6 @@
 from scipy.stats import sobol_indices
 from scipy.stats.qmc import Sobol
-
+import numpy as np
 
 
 
@@ -9,12 +9,85 @@ from scipy.stats.qmc import Sobol
 #------------------------------------------------------------------------------
 
 def sobolSampleGen(ndim, N):
-    """
-    Generate Sobol sequence samples
-    """
+    # Generate Sobol sequence samples
+
+    gen = Sobol(ndim)
+
+    return gen.random(N)
+    # return gen.random_base2(N)
+
+#------------------------------------------------------------------------------
+
+def computeSobolIndices(O_A, O_B, O_AB, ndim, cat):
+    # compute sobol indices from data produced from A, B, and AB sample sets
+    # ndim is input dimensionality
+
+    # assert list(O_A.data.keys()) == list(O_B.data.keys()) == list(O_AB.data.keys())
+    assert O_A.data[cat].shape == O_A.data[cat].shape
+    assert O_AB.data[cat].shape[0] == O_A.data[cat].shape[0]
+    assert O_AB.data[cat].shape[1] == ndim*O_A.data[cat].shape[1]
+
+    odim = O_A.data[cat].shape[0]
+    N_A = O_A.data[cat].shape[1]
+
+    f_A = np.copy(O_A.data[cat])
+    f_B = np.copy(O_B.data[cat])
+    f_AB = np.copy(O_AB.data[cat])
+    f_AB_n = np.reshape(f_AB, [ndim, odim, N_A])
+
+    #TODO FIX: arrays are modified in place by sobol_indices, they are centered
+    # also, results are inconsistent
+    func = {'f_A': f_A, 'f_B': f_B, 'f_AB': f_AB_n}
+    res = sobol_indices(func=func, n=N_A)
+
+    return res.first_order, res.total_order
 
 
 
 
 
 
+
+if __name__ == "__main__":
+
+    from sample_utils import *
+    from scipy.stats import uniform
+
+    def func(x, y):
+        return 3*x + 10*y
+    
+    M = 6
+    N = 2**M
+
+    in_cats = ['x', 'y']
+    in_sizes = [1, 1]
+    ins = SampleData(in_cats, in_sizes)
+    ins.createData(N)
+
+    # create sobol input sets
+    in_A, in_B, in_AB = ins.genSobolData()
+
+    res_A = np.zeros(N)
+    res_B = np.zeros(N)
+    for i in range(N):
+        res_A[i] = func(in_A(i, 'x')[0], in_A(i, 'y')[0])
+        res_B[i] = func(in_B(i, 'x')[0], in_B(i, 'y')[0])
+
+    # should be a dict
+    work = in_AB(list(range(0, N*2)))
+    res_AB = func(work['x'], work['y'])
+
+    out_A = SampleData(['r'], [1], np.atleast_2d(res_A))
+    out_B = SampleData(['r'], [1], np.atleast_2d(res_B))
+    out_AB = SampleData(['r'], [1], np.atleast_2d(res_AB))
+
+    # compute indices
+    r1, r2 = computeSobolIndices(out_A, out_B, out_AB, 2, 'r')
+
+    print(r1)
+    print(r2)
+
+    func2 = lambda x: func(x[0], x[1])
+    print(sobol_indices(func=func2, n=N, dists=[uniform(), uniform()]).first_order)
+
+    breakpoint()
